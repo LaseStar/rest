877768e3ad72192a33f846f13faf774075a606f2
LaseStar <lase_star@mail.ru>
2022-12-12 00:43:01 +0300

    Add_lesson7

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -4,8 +4,11 @@ import React from 'react';
 import AuthorList from './components/Author.js';
 import ProjectList from './components/Project.js';
 import TODOList from './components/Todo.js';
-import axios from 'axios';
+import LoginForm from './components/Auth.js';
+import axios from "axios";
 import {BrowserRouter, Route, Routes, Link, Navigate} from "react-router-dom";
+import Cookies from "universal-cookie"
+
 
 class App extends React.Component {
   constructor(props){
@@ -14,11 +17,55 @@ class App extends React.Component {
         'authors': [],
         'projects': [],
         'todos': [],
+        'token': '',
     }
   }
 
-  componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/authors')
+  logout() {
+        this.set_token('')
+        this.setState({'authors':[]})
+        this.setState({'projects':[]})
+        this.setState({'todos':[]})
+  }
+
+  is_auth() {
+       return !!this.state.token
+  }
+
+  set_token(token) {
+    const cookies = new Cookies()
+    cookies.set('token', token)
+    this.setState({'token':token}, ()=>this.load_data())
+  }
+
+  get_token_storage() {
+      const cookies = new Cookies()
+      const token = cookies.get('token')
+      this.setState({'token':token}, () => this.load_data())
+  }
+
+  get_token(username, password) {
+        const data = {username:username, password:password}
+        axios.post('http://127.0.0.1:8000/api-token/', data).then(response => {
+            this.set_token(response.data['token'])
+        }).catch(error => alert('Wrong login or password'))
+  }
+
+  get_headers() {
+        let headers = {
+            'Content-Type': 'application/json'
+        }
+        if (this.is_auth()){
+            headers['Authorization'] = 'Token '+this.state.token
+        }
+        return headers
+  }
+
+  load_data() {
+
+        const headers = this.get_headers()
+
+        axios.get('http://127.0.0.1:8000/api/authors/', {headers})
         .then(response => {
             const authors = response.data
                 this.setState(
@@ -30,7 +77,7 @@ class App extends React.Component {
         }
         ).catch(error => console.log(error))
 
-        axios.get('http://127.0.0.1:8000/api/projects')
+        axios.get('http://127.0.0.1:8000/api/projects/', {headers})
         .then(response => {
             const projects = response.data
                 this.setState(
@@ -42,7 +89,7 @@ class App extends React.Component {
         }
         ).catch(error => console.log(error))
 
-        axios.get('http://127.0.0.1:8000/api/todo')
+        axios.get('http://127.0.0.1:8000/api/todo/', {headers})
         .then(response => {
             const todos = response.data
                 this.setState(
@@ -53,8 +100,11 @@ class App extends React.Component {
 
         }
         ).catch(error => console.log(error))
-
   }
+
+  componentDidMount() {
+    this.load_data()
+    }
 
 
   render() {
@@ -72,12 +122,15 @@ class App extends React.Component {
                     <li>
                         <Link to='/todos'>ToDos</Link>
                     </li>
-
+                    <li>
+                        {this.is_auth() ? <button onClick={() => this.logout()}>Logout</button> : <Link to='/login'>Login</Link>}
+                    </li>
                 </nav>
                 <Routes>
                     <Route exact path='/' element={<AuthorList authors={this.state.authors}/>}/>
                     <Route exact path='/projects' element={<ProjectList projects={this.state.projects}/>}/>
-                    <Route exact path='/todos' elements={<TODOList todos={this.state.todos}/>}/>
+                    <Route exact path='/todos' element={<TODOList todos={this.state.todos}/>}/>
+                    <Route exact path='/login' element={<LoginForm get_token={(username, password) =>this.get_token(username, password)}/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
